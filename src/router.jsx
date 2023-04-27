3fc1339e00d93849b588227accde16daf3b557c2
GrowMoreSolution <gm.solutions47@gmail.com>
2023-04-27 12:32:42 +0530

    install apex chart

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,17 @@
 import Dashboard from './component/Dashboard/Dashboard';
 import DashboardTemplate from './component/Dashboard/DashboardTemplate';
+import Calender from './component/GridComponent/Calender';
+import Chart from './component/GridComponent/Chart';
+import Gantt from './component/GridComponent/Gantt';
+import Numbers from './component/GridComponent/Numbers';
+import Simpledate from './component/GridComponent/Simpledate';
+import Worlload from './component/GridComponent/Worlload';
 import Maindashboard from './component/Header/Maindashboard';
 
 export default [
   {
     path:'/',
-    element:() => <Dashboard/>,
+    element:() => <Chart/>,
     exact:true,
   },
   {
