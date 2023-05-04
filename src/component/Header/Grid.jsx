7e1b53c6b3208a7bf4a074f818b2b42cd66ccdc9
hassaanasif1998 <hassaanasif1998@hotmail.com>
2023-05-04 10:49:50 +0500

    Update

--- src/component/Header/Grid.jsx
+++ src/component/Header/Grid.jsx
@@ -13,20 +13,20 @@ import { useSelector } from "react-redux";
 import { useEffect } from "react";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
-export default function Grid({SetBoxId}) {
+export default function Grid({ SetBoxId }) {
   const layouts = {
     lg: [{ i: "a", x: 0, y: 0, w: 1, h: 2, data: 0 }],
+    
   };
-  const BoxWidth = useSelector((state)=> state.WidthObj.selectedWId )
+  const BoxWidth = useSelector((state) => state.WidthObj.selectedWId);
 
   const [layout, setLayout] = useState(layouts);
   const [BoxDivId, SetBoxDivId] = useState(null);
-  
 
   const handleDrop = (item, monitor) => {
     const dropPosition = monitor.getClientOffset();
     const dropArea = monitor.getSourceClientOffset();
-    const x = Math.floor((dropPosition.x - dropArea.x) / 100);
+    const x = Math.floor((dropPosition.x - dropArea.x) / 120);
     const y = Math.floor((dropPosition.y - dropArea.y) / 60);
     const currentTime = Date.now();
     const timeString = new Date(currentTime).toLocaleString();
@@ -36,9 +36,10 @@ export default function Grid({SetBoxId}) {
       x,
       y,
       w: 1,
-      h:2
+      h: 6,
     };
-  
+    console.log(newItem);
+
     // Update layouts state with the new item added to the end of the array
     setLayout((prevLayouts) => {
       return {
@@ -55,115 +56,138 @@ export default function Grid({SetBoxId}) {
       canDrop: monitor.canDrop(),
     }),
   }));
-  const SetGrid = (value,id)=>{
+
+  const SetGrid = (value, id) => {
     // var element = document.getElementById(b);
     // element.style.width = "80%";
-    // document.getElementById("b").setAttribute("style","width:25%"); 
-    if(value==1){
-      document.getElementById(`wi-${id}`).style.width='25%';
-    }
-    else if(value==2){
-      document.getElementById(`wi-${id}`).style.width='50%';
-    }
-    else if(value==3){
-      document.getElementById(`wi-${id}`).style.width='100%';
-    }
-   
-  }
+    // document.getElementById("b").setAttribute("style","width:25%");
+    console.log(layout.lg[0].h)
 
-  const Setdis = (value,id)=>{
-    if(value==2){
-    document.getElementById(`di-${id}`).style.display='block'
-    return
+    if (value == 1) {
+      document.getElementById(`wi-${id}`).style.width = "25%";
+    } else if (value == 2) {
+      document.getElementById(`wi-${id}`).style.width = "50%";
+    } else if (value == 3) {
+      document.getElementById(`wi-${id}`).style.width = "100%";
     }
-      document.getElementById(`di-${id}`).style.display='none'
-  }
-  useEffect(()=>{
-    SetGrid(BoxWidth,BoxDivId)
-  },[BoxWidth])
+  };
 
+  const Setdis = (value, id) => {
+    if (value == 2) {
+      document.getElementById(`di-${id}`).style.display = "block";
+      return;
+    }
+    document.getElementById(`di-${id}`).style.display = "none";
+  };
+  useEffect(() => {
+    SetGrid(BoxWidth, BoxDivId);
+  }, [BoxWidth]);
 
   return (
-    <div ref={drop} >
+    <div ref={drop}>
       <ResponsiveGridLayout
         className="layout"
         layouts={layout}
         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-        cols={{ lg: 3, md: 3, sm: 3, xs: 1, xxs: 1 }}
-        // rowHeight={60}
+        cols={{ lg: 4, md: 3, sm: 3, xs: 1, xxs: 1 }}
+        rowHeight={50}
       >
-        {layout.lg.map((item,i) => {
+        {layout.lg.map((item, i) => {
           return (
-          
-            <div key={item.i} id={`wi-${i}`} className="group" onClick={()=> Setdis(1,BoxDivId)} >
-              <button   className={`focus:border focus:border-blue-500 focus:rounded focus:!h-auto w-full wid-${i}`} onClick={(e)=>{SetBoxId(e,2);Setdis(2,i);SetBoxDivId(i)}}>
-              {item.data === 1 ? (
-                <Numbers />
-              ) : item.data === 2 ? (
-                <ChartGrid />
-              ) : item.data === 3 ? (
-                <Simpledata />
-              ) : item.data === 4 ? (
-                <Calender />
-              ) : item.data === 5 ? (
-                <Charteithtext />
-              ) : item.data === 6 ? (
-                <Emailreport />
-              ) : (
-                <Numbers />
-              )}
+            <div
+              key={item.i}
+              id={`wi-${i}`}
+              className="group"
+              onClick={() => Setdis(1, BoxDivId)}
+            >
+              <button
+                className={`focus:border focus:border-blue-500 focus:rounded focus:!h-auto w-full wid-${i}`}
+                onClick={(e) => {
+                  SetBoxId(e, 2);
+                  Setdis(2, i);
+                  SetBoxDivId(i);
+                }}
+              >
+                {item.data === 1 ? (
+                  <Numbers
+                  />
+                ) : item.data === 2 ? (
+                  <ChartGrid />
+                ) : item.data === 3 ? (
+                  <Simpledata />
+                ) : item.data === 4 ? (
+                  <Calender />
+                ) : item.data === 5 ? (
+                  <Charteithtext />
+                ) : item.data === 6 ? (
+                  <Emailreport />
+                ) : (
+                  <Numbers />
+                )}
               </button>
-              <div id={`di-${i}`} className="hidden" >
-              <div className="grid grid-cols-1 sm:grid-cols-3 w-56 rounded-md gap-4 mb-2 bg-white p-2 mt-2 ">
+
+              <div id={`di-${i}`} className="hidden">
+                <div className="grid grid-cols-1 sm:grid-cols-3 w-56 rounded-md gap-4 mb-2 bg-white p-2 mt-2 ">
                   <div>
-                      <div className="relative" onClick={()=>{SetGrid(1,i)}}>
-                        <label className="flex cursor-pointer rounded-md border border-grey/20 bg-white justify-between items-start gap-4 text-center">
-                          <div className="h-8 w-4/12 bg-grey/20 rounded-md"></div>
-                          <input
-                            name="teamsize"
-                            type="radio"
-                            className="form-checkbox relative hidden z-10 peer"
-                          />
-                          <span className="rounded-lg border border-grey/20 peer-checked:border-grey absolute top-0 left-0 z-0 w-full h-full"></span>
-                        </label>
-                        
-                      </div>
-                      <p className="text-sm mt-1" >Small</p>
+                    <div
+                      className="relative"
+                      onClick={() => {
+                        SetGrid(1, i);
+                      }}
+                    >
+                      <label className="flex cursor-pointer rounded-md border border-grey/20 bg-white justify-between items-start gap-4 text-center">
+                        <div className="h-8 w-4/12 bg-grey/20 rounded-md"></div>
+                        <input
+                          name="teamsize"
+                          type="radio"
+                          className="form-checkbox relative hidden z-10 peer"
+                        />
+                        <span className="rounded-lg border border-grey/20 peer-checked:border-grey absolute top-0 left-0 z-0 w-full h-full"></span>
+                      </label>
+                    </div>
+                    <p className="text-sm mt-1">Small</p>
                   </div>
                   <div>
-                      <div className="relative" onClick={()=>{SetGrid(2,i)}}>
-                        <label className="flex cursor-pointer rounded-md border border-grey/20 bg-white justify-between items-start gap-4 text-center">
-                          <div className="h-8 w-6/12 bg-grey/20 rounded-md"></div>
-                          <input
-                            name="teamsize"
-                            type="radio"
-                            className="form-checkbox relative hidden z-10 peer"
-                          />
-                          <span className="rounded-lg border border-grey/20 peer-checked:border-grey absolute top-0 left-0 z-0 w-full h-full"></span>
-                        </label>
-                      </div>
-                      <p className="text-sm mt-1" >Medium</p>
-                    </div>    
-                      <div>
-                      <div className="relative" onClick={()=>{SetGrid(3,i)}}>
-                        <label className="flex cursor-pointer rounded-md border border-grey/20 bg-white justify-between items-start gap-4 text-center">
-                          <div className="h-8 w-full bg-grey/20 rounded-md"></div>
-                          <input
-                            name="teamsize"
-                            type="radio"
-                            className="form-checkbox relative hidden z-10 peer"
-                          />
-                          <span className="rounded-lg border border-grey/20 peer-checked:border-grey absolute top-0 left-0 z-0 w-full h-full"></span>
-                        </label>
-                        
-                      </div>
-                      <p className="text-sm mt-1" >Large</p>
-                      </div>
-                      
-                    </div> 
+                    <div
+                      className="relative"
+                      onClick={() => {
+                        SetGrid(2, i);
+                      }}
+                    >
+                      <label className="flex cursor-pointer rounded-md border border-grey/20 bg-white justify-between items-start gap-4 text-center">
+                        <div className="h-8 w-6/12 bg-grey/20 rounded-md"></div>
+                        <input
+                          name="teamsize"
+                          type="radio"
+                          className="form-checkbox relative hidden z-10 peer"
+                        />
+                        <span className="rounded-lg border border-grey/20 peer-checked:border-grey absolute top-0 left-0 z-0 w-full h-full"></span>
+                      </label>
+                    </div>
+                    <p className="text-sm mt-1">Medium</p>
+                  </div>
+                  <div>
+                    <div
+                      className="relative"
+                      onClick={() => {
+                        SetGrid(3, i);
+                      }}
+                    >
+                      <label className="flex cursor-pointer rounded-md border border-grey/20 bg-white justify-between items-start gap-4 text-center">
+                        <div className="h-8 w-full bg-grey/20 rounded-md"></div>
+                        <input
+                          name="teamsize"
+                          type="radio"
+                          className="form-checkbox relative hidden z-10 peer"
+                        />
+                        <span className="rounded-lg border border-grey/20 peer-checked:border-grey absolute top-0 left-0 z-0 w-full h-full"></span>
+                      </label>
+                    </div>
+                    <p className="text-sm mt-1">Large</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            </div>
-            
           );
         })}
       </ResponsiveGridLayout>
@@ -176,7 +200,8 @@ export default function Grid({SetBoxId}) {
 //     <div style={{ width: "100%", height: "100%", backgroundColor }}>{id}</div>
 //   );
 // }
-          {/* <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-2">
+{
+  /* <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-2">
                       <div className="relative">
                         <label className="flex cursor-pointer rounded-md border border-grey/20 bg-white justify-between items-start gap-4 p-1.5 text-center">
                           <div className="h-12 w-4/12 bg-grey/20 rounded-md"></div>
@@ -210,4 +235,5 @@ export default function Grid({SetBoxId}) {
                           <span className="rounded-lg border border-grey/20 peer-checked:border-grey absolute top-0 left-0 z-0 w-full h-full"></span>
                         </label>
                       </div>
-                    </div> */}
+                    </div> */
+}
